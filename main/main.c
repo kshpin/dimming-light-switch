fe73ee773ef4d4e4b5c5ba4e9f5204c46fdc9eb8
Kirill Shpin <kirill.shpin@gmail.com>
2026-02-25 20:12:28 -0800

    Add logs to business logic

--- main/main.c
+++ main/main.c
@@ -1,7 +1,11 @@
+#include "esp_log.h"
+#include "nvs_flash.h"
+
 #include "led_control.h"
 #include "button.h"
 #include "zigbee.h"
-#include "nvs_flash.h"
+
+#define TAG "MAIN"
 
 #define BUTTON_ON_OFF_GPIO   GPIO_NUM_2
 #define BUTTON_ZIGBEE_GPIO   GPIO_NUM_17
@@ -10,17 +14,24 @@
 static void on_off_button_pressed(void)
 {
     bool new_state = !led_get_on_off();
+
+    ESP_LOGI(TAG, "Manual control button pressed, new state: %s", new_state ? "ON" : "OFF");
+
     led_set_on_off(new_state);
     zigbee_on_off_updated(new_state);
 }
 
 static void on_zigbee_on_off(bool on)
 {
+    ESP_LOGI(TAG, "Zigbee On/Off updated, new state: %s", on ? "ON" : "OFF");
+
     led_set_on_off(on);
 }
 
 static void on_zigbee_level(uint8_t level)
 {
+    ESP_LOGI(TAG, "Zigbee Level updated, new level: %d", level);
+
     led_set_brightness(level);
 }
 
@@ -32,4 +43,6 @@ void app_main(void)
     button_init_long_press(BUTTON_ZIGBEE_GPIO, zigbee_start_steering,
                            zigbee_factory_reset, LONG_PRESS_MS);
     zigbee_init(on_zigbee_on_off, on_zigbee_level);
+
+    ESP_LOGI(TAG, "Init complete");
 }
